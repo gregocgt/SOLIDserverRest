09fa86b722867761e09b9e073087f36647d5bead
gregocgt <gregory.cugatii@gmail.com>
2019-02-21 10:05:55 +0100

    Publishing On PyPI

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(name='SOLIDserverRest',
-    version='1.0',
+    version='1.01',
     description='The SOLIDserverRest is a library to drive EfficientIP API',
-    long_description=long_description,
+    long_description="SOLIDserverRest: This 'SOLIDserverRest' allows to easily interact with SOLIDserver's REST API. It allows managing all IPAM objects through CRUD operations.",
     long_description_content_type="text/markdown",
     url='https://github.com/gregocgt/efficientip_Python_module_SOLIDserver',
     author='Gregory CUGAT',
