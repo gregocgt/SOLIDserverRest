9a00bc33c7aed7cb17cfa70341f0351e2d872c7d
gregocgt <gregory.cugatii@gmail.com>
2019-02-22 14:27:16 +0100

    -

--- test.py
+++ test.py
@@ -7,3 +7,4 @@ def test_answer():
     testR = SOLIDserverRest('192.0.2.42', 'soliduser', 'solidpass')
     parameters = {'site_name': 'toto', 'site_description': 'site description'}
     answerR = testR.query('ip_site_update', parameters)
+    answerR = testR.query('ip_site_delete', parameters)
