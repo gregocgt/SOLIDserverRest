b1d7bdd217820da1514025aec01de6f330772f25
gregocgt <gregory.cugatii@gmail.com>
2019-02-22 15:43:27 +0100

    -

--- setup.py
+++ setup.py
@@ -1,13 +1,35 @@
+import io
+import os
+
 from setuptools import setup
 
-setup(name='SOLIDserverRest',
-    version='1.02',
-    description='The SOLIDserverRest is a library to drive EfficientIP API',
-    long_description="SOLIDserverRest: This 'SOLIDserverRest' allows to easily interact with SOLIDserver's REST API. It allows managing all IPAM objects through CRUD operations.",
-    long_description_content_type="text/markdown",
-    url='https://github.com/gregocgt/efficientip_Python_module_SOLIDserver',
+here = os.path.dirname(__file__)
+
+with open(os.path.join(here, 'README.md')) as f:
+    long_description = f.read()
+
+setup(
+    name='SOLIDserverRest',
+    version='1.04',
     author='Gregory CUGAT',
+    url='https://github.com/gregocgt/efficientip_Python_module_SOLIDserver',
+    description='The SOLIDserverRest is a library to drive EfficientIP API',
+    long_description_content_type="text/markdown",
+    long_description=long_description,
     author_email='gregory.cugat@efficientip.com',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        ],
     license='BSD 2',
     packages=['SOLIDserverRest'],
-    zip_safe=False)
+    zip_safe=False,
+    python_requires=">=3.6",
+    py_modules=['check_python_versions'],
+    entry_points={
+        'console_scripts': [
+            'check-python-versions = check_python_versions:main',
+        ],}
+    )
