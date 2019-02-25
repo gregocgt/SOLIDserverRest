9edb66556bc65bea9c59e4b7457decfc2adcac8c
Alex Chauvin <ach@meta-x.org>
2019-02-25 10:46:14 +0100

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,8 @@
 from SOLIDserverRest.SOLIDserverRest import *
 
 def test_answer():
-    testR = SOLIDserverRest('192.0.2.42', 'soliduser', 'solidpass')
+    testR = SOLIDserverRest('192.0.2.42')
+    testR.use_native_ssd('soliduser', 'solidpass')
     parameters = {'site_name': 'toto', 'site_description': 'site description'}
     answerR = testR.query('ip_site_update', parameters)
     answerR = testR.query('ip_site_delete', parameters)
